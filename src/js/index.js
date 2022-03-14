d2960b3740665f8cf82e94a390cc043dbdb196eb
Osmar trojilio <80086189+osmartrojilio@users.noreply.github.com>
2022-03-13 22:22:12 -0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import '../css/styles.scss';  
+import img from '../assets/bg.jpg';
+
+const textComponent = () => {
+    const elh1 = document.createElement('h1');
+    elh1.innerHTML = 'Olá mundo!';
+    elh1.classList.add('title');
+    return elh1;
+}
+const imgComponent = () => {
+    const elimg = new Image(300, 150);
+    elimg.src = img;
+    return elimg;
+}
+document.body.appendChild(textComponent());
+document.body.appendChild(imgComponent());
